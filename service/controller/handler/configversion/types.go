da0e9565481e5877ed02c1bde0a4c01645d2b58b
kubasobon <wtty.fool@gmail.com>
2021-01-08 12:41:55 +0100

    remove unused fields

--- service/controller/handler/configversion/types.go
+++ service/controller/handler/configversion/types.go
@@ -1,19 +1,10 @@
 package configversion
 
 type Index struct {
-	APIVersion string                  `json:"apiVersion"`
-	Entries    map[string][]IndexEntry `json:"entries"`
+	Entries map[string][]IndexEntry `json:"entries"`
 }
 
 type IndexEntry struct {
-	APIVersion    string   `json:"apiVersion"`
-	AppVersion    string   `json:"appVersion"`
-	ConfigVersion string   `json:"configVersion,omitempty"`
-	Created       string   `json:"created"`
-	Description   string   `json:"description"`
-	Digest        string   `json:"digest"`
-	Home          string   `json:"home"`
-	Name          string   `json:"name"`
-	Urls          []string `json:"urls"`
-	Version       string   `json:"version"`
+	ConfigVersion string `json:"configVersion,omitempty"`
+	Version       string `json:"version"`
 }
