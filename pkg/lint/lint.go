7f7fb8d652a44cffc071b7c16a2e4d1ee0433663
kubasobon <wtty.fool@gmail.com>
2021-02-05 15:38:35 +0100

    change function order

--- pkg/lint/lint.go
+++ pkg/lint/lint.go
@@ -13,10 +13,10 @@ const (
 type LinterFunc func(d *Discovery) (messages LinterMessages)
 
 var AllLinterFunctions = []LinterFunc{
+	LintUnusedConfigValues,
 	LintDuplicateConfigValues,
 	LintOvershadowedConfigValues,
 	LintUnusedConfigPatchValues,
-	LintUnusedConfigValues,
 	LintUndefinedTemplateValues,
 	LintUndefinedTemplatePatchValues,
 }
