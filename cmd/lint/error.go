76fa97a92aebe1aa796d2d5e5d6f599fdb008495
kubasobon <wtty.fool@gmail.com>
2021-02-08 17:33:37 +0100

    make linter happy (ironic)

--- cmd/lint/error.go
+++ cmd/lint/error.go
@@ -2,16 +2,6 @@ package lint
 
 import "github.com/giantswarm/microerror"
 
-// executionFailedError should never be matched against and therefore there is
-// no matcher implement. For further information see:
-//
-//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
-//
-// nolint:unused
-var executionFailedError = &microerror.Error{
-	Kind: "executionFailedError",
-}
-
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
